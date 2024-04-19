252212dd9f921a1c869ebd0e7dbcb8987695f357
BMBachir <boubaidjabachir3@gmail.com>
2024-04-19 17:35:20 +0100

    Initial commit

---
+++ src/routes/Routers.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
+import Doctors from "../pages/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/doctor-details" element={<DoctorDetails />} />
+    </Routes>
+  );
+};
+
+export default Routers;
